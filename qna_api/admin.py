45f2c8a24171e19f9f56c1023f059fe88da67553
Kailash Anantha <avnkailash@gmail.com>
2024-01-23 17:11:33 +0530

    Created the first basic version of the API with user profile model and registered it in the admin.

---
+++ qna_api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from qna_api import models
+
+admin.site.register(models.UserProfile)
---
+++ qna_api/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of user"""
+        return self.email
+
+
+# class Question(models.Model):
+#     """Database model for questions in the system"""
+
+#     title = models.CharField(max_length=255)
+#     body = models.TextField()
+#     user = models.ForeignKey(
+#         UserProfile, on_delete=models.CASCADE, related_name="questions"
+#     )
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     tags = models.ManyToManyField("Tag", related_name="questions")
+
+#     def __str__(self):
+#         """Return string representation of question"""
+#         return self.title
